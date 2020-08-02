517a582f4f2fe560d0b9318190cf32106c1daa3a
M95N1K <mysnik999@gmail.com>
2020-08-02 12:09:40 +0600

    Add CryptDLL

---
+++ Code/PassCode.cs
@@ -0,0 +1,45 @@
+﻿namespace Code
+{
+    public static class PassCode
+    {
+        private static string GetRepeatKey(string s, int n)
+        {
+            string result = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                result += ((char)(s[i] ^ s[i % s.Length]));
+            }
+            return result;
+        }
+
+        private static string Enciper(string pass, string key)
+        {
+            string currentKey = GetRepeatKey(key, pass.Length);
+            string res = currentKey[0].ToString();
+            for (int i = 0; i < pass.Length; i++)
+            {
+                res += ((char)(pass[i] ^ currentKey[i % currentKey.Length])).ToString();
+            }
+            res += currentKey[1].ToString();
+            return res;
+        }
+
+        private static string Deciper(string pass, string key)
+        {
+            string tmp = pass.Substring(1, pass.Length - 1);
+            pass = tmp;
+            string currentKey = GetRepeatKey(key, pass.Length);
+            string res = string.Empty;
+            for (int i = 0; i < pass.Length; i++)
+            {
+                res += ((char)(pass[i] ^ currentKey[i % currentKey.Length])).ToString();
+            }
+            return res;
+        }
+
+        public static string Encrypt(string plainText, string key)
+            => Enciper(plainText, key);
+        public static string Decrypt(string encryptedText, string key)
+            => Deciper(encryptedText, key);
+    }
+}
